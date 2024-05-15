a6b56031300b81e64db7bd0b02b39196d6fae3b0
vukas82 <vukas82@gmail.com>
2024-05-15 13:11:16 +0200

    fix

--- components/LandingPage.jsx
+++ components/LandingPage.jsx
@@ -46,16 +46,16 @@ const LandingPage = ({ text }) => {
         // className=" w-full"
       />
       <div className=" w-full h-auto z-10 flex items-center justify-between flex-col">
-        {parts.map((part, index) => (
+        <h1>{parts}</h1>
+        {/* {parts.map((part, index) => (
           <h1
             className="text-4xl md:text-4xl lg:text-5xl overflow-hidden  text-center font-bold text-white p-2"
             key={index}
           >
             {part}
             {index !== parts.length - 1 && <br />}{" "}
-            {/* Dodajemo <br /> između delova osim za poslednji deo */}
           </h1>
-        ))}
+        ))} */}
 
         <p className="text-xl md:text-2xl overflow-hidden font-normal text-white text-center py-10 md:py-14 lg:py-24 w-3/4 md:w-2/4">
           {offerText}
