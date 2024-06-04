46446c846a08fee253028fa8e08d723066cf2241
vukas82 <vukas82@gmail.com>
2024-06-04 14:11:02 +0200

    upgrade next

--- server.js
+++ server.js
@@ -53,8 +53,8 @@ const { parse } = require("url");
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
-// const hostname = "localhost";
-const hostname = "spinlogicslots.com";
+const hostname = "localhost";
+// const hostname = "spinlogicslots.com";
 // const port = 3000
 const port = process.env.PORT || 3000;
 // when using middleware `hostname` and `port` must be provided below
